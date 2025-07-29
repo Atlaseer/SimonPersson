e05ab7cbcc9992be27ef40707de4951e7c1c8fb5
Simon Persson <atlaspersson@outlook.com>
2025-07-30 00:23:27 +0200

    Created Webpage.jsx with imported script

--- client/src/Pages/Webpage.jsx
+++ client/src/Pages/Webpage.jsx
@@ -1,10 +1,19 @@
-import '../Styles/webpage.css'
+import { useEffect } from 'react';
+import '../Styles/webpage.css';
+import BookScript from '../Scripts/BookScript.js';
 
 function WebPage() {
+    useEffect(() => {
+        BookScript();
+    }, []);
     return(
       
         <main className="homepage">
-                <img src="assets/face-img.jpg" alt="corner-pic" className="corner-img" />
+                <img 
+                src="assets/face-img.jpg" 
+                alt="corner-pic" 
+                className="corner-img" 
+                />
                 <div class="book">
                 <div class="page left-page" id="leftPage">Left-page</div>
                 <div class="page right-page" id="rightPage">Right-page</div>
