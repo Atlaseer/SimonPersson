9429d4af22ffd21b8a8569d1e2c51c2df426c98c
Simon Persson <atlaspersson@outlook.com>
2025-07-30 00:46:27 +0200

    Made changes in App.jsx to work with new page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 import './Styles/webpage.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Header from './Components/Header.jsx';
-import WebPage from './Pages/Homepage.jsx';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Components/header.jsx';
+import WebPage from './Pages/Webpage.jsx';
 
 
 function App() {
 
   return (
-    <>
-      <div>
-        <h1>This is a title</h1>
-      </div>
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<WebPage/>} />
+    </Routes>
+    <Footer/>
 
-  </>
+  </BrowserRouter>
   )
 }
 
