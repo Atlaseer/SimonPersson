b4ece3a6045e7af8106d1b134792274d72662d82
Simon Persson <atlaspersson@outlook.com>
2025-07-31 23:54:43 +0200

    Made left page nr 0 invis, and right page length -1 invis

--- client/src/Scripts/BookScript.js
+++ client/src/Scripts/BookScript.js
@@ -13,6 +13,19 @@ export function BookScript() {
   function updatePages() {
     leftPage.textContent = pages[currentPage].left;
     rightPage.textContent = pages[currentPage].right;
+
+    if(currentPage === 0) {
+      leftPage.style.visibility = "hidden";
+    } else {
+      leftPage.style.visibility = "visible";
+    }
+
+    if(currentPage === pages.length - 1) {
+      rightPage.style.visibility = "hidden";
+    } else {
+      rightPage.style.visibility = "visible";
+    }
+
   }
 
   if (leftPage && rightPage) {
