946428f947f55a48a444ecea7fde9019785c36de
“Simon <atlaspersson@outlook.com>
2025-08-29 11:49:57 +0200

    Moved pages function to Webpage.jsx with react, instead of js. Created Projects and AboutMe page

--- client/src/Pages/Webpage.jsx
+++ client/src/Pages/Webpage.jsx
@@ -1,26 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import '../Styles/webpage.css';
-import {BookScript} from '../Scripts/BookScript.js';
+//import {BookScript} from '../Scripts/BookScript.js';
 import faceImg  from '../assets/face-img.jpg';
 
 function WebPage() {
-    useEffect(() => {
-        console.log("WebPage component mounted");
-        BookScript();
-    }, []);
+    
+    const[currentPage, setCurrentPage] = useState(0)
+
+    const pages= [
+        { left: "", right: "Hello! I´m Simon"},
+        { left: "I´ve worked on these projeccts", right: "Which has taught me"},
+        { left: "Fin", right: ""}
+    ];
+
+    const nextPage = () => currentPage < pages.length -1 && setCurrentPage(currentPage +1);
+    const prevPage = () => currentPage > 0 && setCurrentPage(currentPage - 1);
 
     return(
-     
         <main className="homepage">
-                <img 
-                    src={faceImg} 
-                    alt="corner-pic" 
-                    className="corner-img" 
-                />
-                <div className="book">
-                    <div className="page left-page" id="leftPage">Left-page</div>
-                    <div className="page right-page" id="rightPage">Right-page</div>
+            <img src={faceImg} alt="corner-pic" className="corner-img"/>
+            <div className="book">
+                <div className="page left-page" onClick={prevPage}>
+                    {pages[currentPage].left}
                 </div>
+                <div className="page right-page" onClick={nextPage}>
+                    {pages[currentPage].right}
+                </div>
+            </div>
         </main>
         
     );
