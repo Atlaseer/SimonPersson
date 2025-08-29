e5d9dbd06cdf0690543109632505bc8a014b8b9b
“Simon <atlaspersson@outlook.com>
2025-08-29 16:08:33 +0200

    Updated Book.jsx name and references

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,9 @@ import './Styles/index.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './Components/Header.jsx';
 import Footer from './Components/Footer.jsx';
-import WebPage from './Pages/Webpage.jsx';
+import Book from './Pages/Book.jsx';
+import AboutMe from './Pages/AboutMe.jsx';
+import Projects from './Pages/Projects.jsx';
 
 
 function App() {
