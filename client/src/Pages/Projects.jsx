7819c67f410e955a37b832521ddd4ad8000f2adb
“Simon <atlaspersson@outlook.com>
2025-09-01 01:38:40 +0200

    Updating Projects.jsx

--- client/src/Pages/Projects.jsx
+++ client/src/Pages/Projects.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 import '../Styles/index.css';
+import '../Styles/projects.css';
 
 function Projects() {
     return(
-        <h2>These are the projects I've been working on!</h2>
+        <main className="project-page">
+          <h2>These are the projects I've been working on!</h2>
+
+        </main>
     )
 }
 
