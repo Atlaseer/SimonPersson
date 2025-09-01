7cd857b04763fda567ce66fada3c1042465e9e43
“Simon <atlaspersson@outlook.com>
2025-09-01 23:52:57 +0200

    Created boxes for project-pages

--- client/src/Pages/Projects.jsx
+++ client/src/Pages/Projects.jsx
@@ -5,7 +5,16 @@ import '../Styles/projects.css';
 function Projects() {
     return(
         <main className="project-page">
-          <h2>These are the projects I've been working on!</h2>
+                    <h2>These are the projects I've been working on!</h2>
+
+          <div className='project-grid'>
+            <div className='project-class'>
+              This is a project
+            </div>
+                  <div className='project-class'>
+              This is a project
+            </div>
+          </div>
 
         </main>
     )
