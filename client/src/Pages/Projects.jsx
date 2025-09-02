089dc3f896d6c870046d2f9e3281d68a140c415c
“Simon <atlaspersson@outlook.com>
2025-09-02 15:45:31 +0200

    Fixed text to be left-aligned

--- client/src/Pages/Projects.jsx
+++ client/src/Pages/Projects.jsx
@@ -1,7 +1,11 @@
 import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import '../Styles/index.css';
 import '../Styles/projects.css';
-import projectPic1 from '../assets/woods2.jpg'
+import projectPic1 from '../assets/woods.jpg'
+import projectPic2 from '../assets/woods2.jpg'
+import projectPic3 from '../assets/woods3.jpg'
+
 
 function Projects() {
     return(
@@ -11,25 +15,25 @@ function Projects() {
           <div className='project-grid'>
             
             <div className='project-class'>
-              <h2 className='project-title'>Project Title</h2>
+              <h2 className='project-title'>Work-It</h2>
               <img src={projectPic1} alt="project-pic" />
-              <p className='location'>Location: Kristianstad University</p>
-              <p className='description'>Description of app made by Simon</p>
+              <p className='location'>Project: Hobby</p>
+              <p className='description'>App currently in development</p>
             </div>
 
             <div className='project-class'>
-              <h2 className='project-title'>Project title</h2>
-              <img src={projectPic1} alt="project-pic" />
-              <p className='location'>Location: Kristianstad University</p>
-              <p className='description'>Description</p>
+              <h2 className='project-title'>FoodLovers</h2>
+              <img src={projectPic2} alt="project-pic" />
+              <p className='location'>Project: Kristianstad University</p>
+              <p className='description'>App developed in a team using MERN-stack</p>
 
             </div>
 
             <div className='project-class'>
-              <h2 className='project-title'>Project title</h2>
-              <img src={projectPic1} alt="project-pic" />
-              <p className='location'>Location: Kristianstad University</p>
-              <p className='description'>Description</p>
+              <h2 className='project-title'>Calculator</h2>
+              <img src={projectPic3} alt="project-pic" />
+              <p className='location'>Project: Practice</p>
+              <p className='description'>Introduction to HTML, CSS and Javascript</p>
             </div>
 
           </div>
