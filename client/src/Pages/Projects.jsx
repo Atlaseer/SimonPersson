4a5129641efe320f88ad8debbeaec129aa8394f5
“Simon <atlaspersson@outlook.com>
2025-09-02 11:46:17 +0200

    Pic displayed disproportionally

--- client/src/Pages/Projects.jsx
+++ client/src/Pages/Projects.jsx
@@ -1,19 +1,28 @@
 import { useEffect, useState } from 'react';
 import '../Styles/index.css';
 import '../Styles/projects.css';
+import projectPic1 from '../assets/woods2.jpg'
 
 function Projects() {
     return(
         <main className="project-page">
-                    <h2>These are the projects I've been working on!</h2>
+                    <h2>These are some of the projects I've been working on!</h2>
 
           <div className='project-grid'>
+            
             <div className='project-class'>
-              This is a project
+              <h2 className='project-title'>Project Title</h2>
+              <img src={projectPic1} alt="project-pic" />
             </div>
-                  <div className='project-class'>
-              This is a project
+
+            <div className='project-class'>
+              <h2 className='project-title'>Project title</h2>
             </div>
+
+            <div className='project-class'>
+              <h2 className='project-title'>Project title</h2>
+            </div>
+
           </div>
 
         </main>
