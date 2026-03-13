56ef22a2ac857263ccab89c8f713f160fb1e38b7
“Simon <atlaspersson@outlook.com>
2026-03-13 02:47:17 +0100

    Updated hoover effects

--- client/src/Pages/AboutMe.jsx
+++ client/src/Pages/AboutMe.jsx
@@ -11,12 +11,12 @@ function AboutMe () {
               </div>
             <div className='about-box'>
                 <h1>Hello I'm Simon</h1>
-                <div className="about-text">
+                <div className="about-text1">
                     <h3>Welcome to my portfolio!</h3> 
                     <h3>I'm a passionate developer 
                         with experience in creating engaging web applications.</h3>
                 </div>
-                <div className='about-text'>
+                <div className='about-text2'>
                     <h3>Here's a bit about my background and skills:</h3>
                     <h3>I have a strong foundation in software engineering 
                         from a bachelor's degree in computer science!</h3>
